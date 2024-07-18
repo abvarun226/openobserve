f498dc194b1a90229475c889ed7980fc76db4b26
Bhargav <BJP232004@GMAIL.COM>
2024-07-18 16:23:22 +0530

    added root resources

--- web/src/main.ts
+++ web/src/main.ts
@@ -131,7 +131,7 @@ if ('serviceWorker' in navigator) {
   //   // });
   // }
   navigator.serviceWorker.register('./assets/sw.js', {
-    scope: '/web/'
+    scope: '/web/assets/'
   }).then(registration => {
       console.log('Service Worker registered:', registration);
 
